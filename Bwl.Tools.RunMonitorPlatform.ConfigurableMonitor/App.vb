a43166400acb39d465baa3d1bbffd503e7064c88
Igor Koshelev <igor@lifemotion.ru>
2016-01-20 18:16:26 +0300

    Возможность настройки с помощью текстовых файлов.

---
+++ Bwl.Tools.RunMonitorPlatform.ConfigurableMonitor/App.vb
@@ -0,0 +1,113 @@
+﻿Imports Bwl.Tools.RunMonitorPlatform
+
+Public Module App
+    Dim _logger As New Logger
+    Dim _core As New RunMonitorCore(_logger)
+    Dim _form As New RunMonitorStatus(_logger)
+    Dim _basedir = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..")
+
+    Sub Main()
+        Application.EnableVisualStyles()
+        WriteHelp()
+        LoadTasks()
+        _form.Show()
+        _form.Tasks = _core.Tasks.ToArray
+        _core.RunInThread()
+        Application.Run(_form)
+        _core.StopThread()
+    End Sub
+
+    Private Sub WriteHelp()
+        Dim file = IO.File.CreateText(IO.Path.Combine(_basedir, "Help.txt"))
+        file.WriteLine("Each Task is *.txt file")
+        file.WriteLine("First line - task type, other - pairs of argument=value")
+        file.WriteLine()
+        file.WriteLine("ProcessTask")
+        file.WriteLine("ExecutableFileName = calc.exe")
+        file.WriteLine("Arguments = ")
+        file.WriteLine("ProcessName = Calculator")
+        file.WriteLine("RestartDelaySecongs = 15")
+        file.WriteLine("WorkingDirectory = ")
+        file.WriteLine("NetClientCheck = host = port")
+        file.WriteLine()
+        file.WriteLine("MemWatcherTask")
+        file.WriteLine("Limit = 1500")
+        file.WriteLine()
+        file.WriteLine("NetWatcherTask")
+        file.WriteLine("Address = https://ya.ru")
+        file.WriteLine("GetOnlyHeaders = True")
+        file.Close()
+    End Sub
+
+    Private Sub LoadTasks()
+        Dim taskFiles = IO.Directory.GetFiles(_basedir, "*.txt", IO.SearchOption.AllDirectories)
+        For Each file In taskFiles
+            Try
+                Dim lines = IO.File.ReadAllLines(file, Text.Encoding.UTF8)
+                If lines(0) = "ProcessTask" Then
+                    Dim parameters As New ProcessTaskParameters
+                    Dim addChecks As New List(Of ITaskCheck)
+                    For Each line In lines
+                        Dim parts = line.Split("=")
+                        If parts.Length >= 2 Then
+                            Dim argument = parts(0).Trim.ToLower
+                            Dim value = parts(1).Trim
+                            If argument = "ExecutableFileName".ToLower Then parameters.ExecutableFileName = value
+                            If argument = "Arguments".ToLower Then parameters.Arguments = value
+                            If argument = "ProcessName".ToLower Then parameters.ProcessName = value
+                            If argument = "RestartDelaySecongs".ToLower Then parameters.RestartDelaySecongs = Val(value)
+                            If argument = "WorkingDirectory".ToLower Then parameters.WorkingDirectory = value
+                            If argument = "NetClientCheck".ToLower Then
+                                If parts.Length >= 3 Then
+                                    Dim host = parts(1).Trim
+                                    Dim port = Val(parts(2))
+                                    addChecks.Add(New NetClientCheck(host, port,
+                                                 Sub(check As NetClientCheck)
+                                                     Dim result = check.NetClient.SendMessageWaitAnswer(New NetMessage("S", "testrequest"), "testanswer", 5)
+                                                     If result Is Nothing Then Throw New TaskCheckException(Nothing, check, "No Net Answer!")
+                                                 End Sub))
+                                End If
+                            End If
+                        End If
+                    Next
+                    Dim task = New ProcessTask(parameters, addChecks)
+                    _core.Tasks.Add(task)
+                End If
+
+                If lines(0) = "MemWatcherTask" Then
+                    Dim limit = 1500
+                    For Each line In lines
+                        Dim parts = line.Split("=")
+                        If parts.Length = 2 Then
+                            Dim argument = parts(0).Trim.ToLower
+                            Dim value = parts(1).Trim
+                            If argument = "Limit".ToLower Then limit = Val(value)
+                        End If
+                    Next
+                    Dim task = New MemWatcherTask(limit)
+                    _core.Tasks.Add(task)
+                End If
+
+                If lines(0) = "NetWatcherTask" Then
+                    Dim address = "https://ya.ru"
+                    Dim getOnlyHeaders = True
+
+                    For Each line In lines
+                        Dim parts = line.Split("=")
+                        If parts.Length = 2 Then
+                            Dim argument = parts(0).Trim.ToLower
+                            Dim value = parts(1).Trim
+                            If argument = "Address".ToLower Then address = value
+                            If argument = "GetOnlyHeaders".ToLower Then getOnlyHeaders = CType(value, Boolean)
+                        End If
+                    Next
+
+                    Dim task = New NetWatcherTask(address, getOnlyHeaders)
+                    _core.Tasks.Add(task)
+                End If
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+
+End Module
---
+++ Bwl.Tools.RunMonitorPlatform.ConfigurableMonitor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Bwl.Tools.RunMonitorPlatform.ConfigurableMonitor")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Bwl.Tools.RunMonitorPlatform.ConfigurableMonitor")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("51d4d90c-bbff-4cfc-be7d-434d5bf1d150")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
