1b217a0d3e7fb214e4edfd87ce75c0538868aa04
Igor Koshelev <igor@cleverflow.ru>
2016-09-06 19:59:17 +0300

    (no message)

---
+++ Bwl.Tools.RunMonitorPlatform.RemotingHost/App.vb
@@ -0,0 +1,16 @@
+﻿Imports Bwl.Tools.RunMonitorPlatform
+Imports Bwl.Framework
+Imports Bwl.Network.ClientServer
+
+Public Module App
+    Dim _logger As New Logger
+    Dim _core As New RunMonitorCore(_logger)
+    Dim _autoui As New RunMonitorAutoUI(_logger)
+    Dim _basedir = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..")
+
+    Sub Main()
+        _autoui.Tasks = _core.Tasks.ToArray
+        _core.RunInThread()
+    End Sub
+
+End Module
